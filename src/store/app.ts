23ac58bae376f9f201a44cd1b8be54ee83020927
shikou0918 <shikou.0918@gmail.com>
2023-11-19 18:10:07 +0900

    コーディング練習

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,8 +1,33 @@
-// Utilities
 import { defineStore } from 'pinia'
+import { ref, Ref } from 'vue'
 
-export const useAppStore = defineStore('app', {
-  state: () => ({
-    //
-  }),
+// 型定義
+interface Task {
+  id: number;
+  name: string;
+}
+
+export const useAppStore = defineStore('app', () => {
+  const tasks: Ref<Task[]> = ref([]);
+  let serialId = 0;
+
+  /**
+   * タスクを追加する.
+   * @param name タスク名
+   */
+  const addTask = (name: string): void => {
+    serialId++;
+    tasks.value.push({ id: serialId, name: name })
+  }
+
+  /**
+   * タスクを削除する.
+   * @param id 削除するタスクid
+   */
+  const deleteTask = (id: number): void => {
+    tasks.value = tasks.value.filter(task => task.id !== id);
+
+  }
+
+  return { tasks, addTask, deleteTask }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <script lang="ts" setup>
 import TaskForm from "../components/TaskForm.vue";
 import TaskItem from "../components/TaskItem.vue";
+import { useAppStore } from "../store/app";
+const store = useAppStore();
 </script>
 
 <template >
   <v-container>
     <h1>Todo App</h1>
-    <TaskForm />
-    <TaskItem />
+    <TaskForm @submit="store.addTask" />
+    <!-- for文でタスクを表示させる -->
+    <template v-for="task in store.tasks" :key="task.taskId">
+      <TaskItem :taskId="task.id" :name="task.name" @deleteTask="store.deleteTask" />
+    </template>
   </v-container>
 </template>
